f73f78402d7f9724303b76c4fa5acf15915848db
Matthias Kramer <45616374+MatthiasKramer@users.noreply.github.com>
2018-12-07 08:32:03 +1000

    Add files via upload

---
+++ ROC.m
@@ -0,0 +1,39 @@
+function [ufilt,spikes] = ROC(u)
+% Robust Outlier Cutoff based on the Maximum Absolute Deviation and the 
+% Universal Threshold
+%   23/04/2018
+% u: velocity vector
+
+k = 1.483; % based on a normal distro, see Rousseeuw and Croux (1993)
+
+% Robust estimation of the variance:
+umed = nanmedian(u);
+ustd = k * nanmedian( abs(u - umed));
+% Universal threshold:
+N = length(u);
+lambdau = sqrt(2*log(N));
+
+i_rep = zeros(N, 1);
+
+for i = 1:N
+   
+    if abs((u(i)-umed)/(lambdau*ustd)) > 1.0
+        
+        ufilt(i) = nan;
+        i_rep(i) = 1;
+        
+    else
+        
+        ufilt(i) = u(i);
+        
+    end
+end
+
+spikes = sum(i_rep)/N*100;
+
+%fprintf('ROC, Spikes(percent): %2.8f %%\n', spikes)
+
+% Additional data:
+
+end
+
---
+++ windows.m
@@ -0,0 +1,15 @@
+function [nwindows,start,stop,t] = windows(ChordA2,ChordW2,nParticles,fsample)
+
+ nwindows = min(floor(length(ChordA2)/nParticles),floor(length(ChordW2)/nParticles));
+
+  for i=1:1:nwindows  %adaptive windows
+   start(i) = sum(ChordA2(1:nParticles*i))+sum(ChordW2(1:nParticles*i));
+   stop(i) = sum(ChordA2(1:nParticles*i))+sum(ChordW2(1:nParticles*i));  
+  end
+  
+ start = circshift(start,1);
+ start(1)=1; %first time window
+ stop(length(stop))=sum(ChordW2)+sum(ChordA2); %+1; %adding the last segment
+ t = round((start+stop)./2)/fsample; %calculation of time window centres
+ 
+end
