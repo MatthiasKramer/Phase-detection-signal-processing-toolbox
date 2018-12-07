077d342b983f4d6cd971c22be13fd10b704d4d04
Daniel Valero <45652473+davahue@users.noreply.github.com>
2018-12-07 08:23:30 +0100

    Update chord.m

--- chord.m
+++ chord.m
@@ -1,6 +1,6 @@
 function [ChordW,ChordA,F] = chord(Signal,tmeasure)
 
- %bubble frequency
+ %Initializing parameters
   lengthair = 1;
   counterair = 1;
   lengthwater = 1;
@@ -8,18 +8,18 @@ function [ChordW,ChordA,F] = chord(Signal,tmeasure)
   
   %scanning Signal1
  for k = 1:1:size(Signal,2)-1
-    if Signal(k) == 0         
-        if Signal(k+1) == 0                
+    if Signal(k) == 0 % Tip is in the air...
+        if Signal(k+1) == 0 % ... and will keep on being in the air. 
          lengthair = lengthair+1;
-        elseif Signal(k+1) == 1             
+        elseif Signal(k+1) == 1 % ... and gets wet in the next measurement.
          ChordA(counterair) = lengthair;
          counterair = counterair +1;
          lengthair = 1;                      
         end  
-    elseif Signal(k) == 1 
-        if Signal(k+1) == 1
+    elseif Signal(k) == 1 % Tip is in the water...
+        if Signal(k+1) == 1 % ... and will keep on being in the water.
         lengthwater = lengthwater+1; 
-        elseif Signal(k+1) == 0 
+        elseif Signal(k+1) == 0 % and gets dry in the next measurement.
          ChordW(counterwater) = lengthwater;
          counterwater = counterwater +1;
          lengthwater = 1;
@@ -27,6 +27,6 @@ function [ChordW,ChordA,F] = chord(Signal,tmeasure)
     end     
  end
   
- F = (size(ChordA,2)/(tmeasure)); %1/s 
+ F = (size(ChordA,2)/(tmeasure)); % Bubble frequency (1/s)
  
 end
