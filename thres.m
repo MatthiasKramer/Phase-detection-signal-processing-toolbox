df2585c22b2a50e0c7bf548b1e164f589b3cd98d
Daniel Valero <45652473+davahue@users.noreply.github.com>
2018-12-07 08:41:14 +0100

    Update thres.m

--- thres.m
+++ thres.m
@@ -1,24 +1,25 @@
 function [modeair,modewater,threshold,C,Signalout] = thres(Signal,nbins)
+% Single thresholding for void fraction estimation.
 
- [N,centers]  = hist(Signal,nbins);
- thres = (min(centers)+max(centers))/2; 
- Nair = mean(max(N(centers<thres)));
- Nwater = mean(max(N(centers>thres)));
- modeair = mean(centers(N==max(N(centers<thres))));
- modewater = mean(centers(N==max(N(centers>thres))));
- threshold = (modeair + modewater)/2;    
+[N,centers]  = hist(Signal,nbins);
+thres = (min(centers)+max(centers))/2;
+Nair = mean(max(N(centers<thres)));
+Nwater = mean(max(N(centers>thres)));
+modeair = mean(centers(N==max(N(centers<thres))));
+modewater = mean(centers(N==max(N(centers>thres))));
+threshold = (modeair + modewater)/2;    
 
- %binarization of the signal 
- Signalout(Signal > threshold) = 1;
- Signalout(Signal < threshold) = 0;
+%binarization of the signal (Signalout = binarized signal)
+Signalout(Signal > threshold) = 1; % Water
+Signalout(Signal < threshold) = 0; % Air
  
- %add one value for analyzing air/water chord
-  if Signalout(size(Signalout,2)) == 1
-      Signalout(size(Signalout,2)+1) = 0;
-    else    
-     Signalout(size(Signalout,2)+1) = 1;    
-  end
-  
- C=sum(Signal<threshold)/length(Signal); 
+%add one value for analyzing air/water chord
+if Signalout(size(Signalout,2)) == 1
+    Signalout(size(Signalout,2)+1) = 0;
+else    
+    Signalout(size(Signalout,2)+1) = 1;    
+end
+
+C=sum(Signal<threshold)/length(Signal); 
 
 end
