205ceb3fab98627f7f776b743b35be1695d4f37e
Daniel Valero <45652473+davahue@users.noreply.github.com>
2021-04-29 14:20:14 +0200

    Update Fun.m

--- Fun.m
+++ Fun.m
@@ -1,4 +1,5 @@
 function X = Fun(ud0, C, F, cVM, b, dProbe, dx, gamma, rho_c, rho_d, sigma, nu_c, Cp, T1meas)
+    % FUN Definition of the function to solve numerically.
 
     % X = optimization objective 
     X = 2*dx/T1meas - ud0 - fun_ud(ud0, C, F, cVM, b, dProbe, gamma, rho_c, rho_d, sigma, nu_c, Cp, T1meas);
