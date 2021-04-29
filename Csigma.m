777890e4505f7d0babd05bb1c5ec0bb0838b9c0a
Daniel Valero <45652473+davahue@users.noreply.github.com>
2021-04-29 16:12:12 +0200

    Update Csigma.m

--- Csigma.m
+++ Csigma.m
@@ -1,5 +1,5 @@
 function [Cs] = Csigma(ud, dp, nu_c)
-% calculate C_{\sigma} after Eq. (15)
+% CSIGMA  This function calculates C_{\sigma} parameter after Eq. (15) of Hohermuth et al. (2021)
 a = -7.2*10^(-8); b = 0.002; c = -0.589;
 Rb = dp.*ud./nu_c;
 Cs = max(0,a*Rb.^2+b.*Rb+c);
